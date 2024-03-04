15a1109baf113025342b54d2b8590b19ed5050b1
Himanshu <himanshuraj1209@gmail.com>
2024-03-04 22:34:05 +0530

    [UPD] custom_render.js: Optimize customRender method

--- custom_react.js
+++ custom_react.js
@@ -1,8 +1,16 @@
 function customRender(reactElement, container) {
-   const domElement = document.createElement(reactElement['type']);
-   domElement.innerHTML = reactElement['childern'];
-   domElement.setAttribute('href', reactElement.props.href);
-   domElement.setAttribute('target', reactElement.props.target);
+   // const domElement = document.createElement(reactElement['type']);
+   // domElement.innerHTML = reactElement['childern'];
+   // domElement.setAttribute('href', reactElement.props.href);
+   // domElement.setAttribute('target', reactElement.props.target);
+   // container.appendChild(domElement)
+
+   const domElement = document.createElement(reactElement.type);
+   domElement.innerHTML = reactElement.childern
+   for (const prop in reactElement.props) {
+      if (prop == 'childern') continue;
+      domElement.setAttribute(prop, reactElement.props[prop]);
+   }
    container.appendChild(domElement)
 }
 
@@ -12,7 +20,7 @@ const reactElement = {
       href: 'https://google.com',
       target: '_blank'
    },
-   childern: 'Click me to visit google',
+   childern: 'Click to visit',
 };
 
 const mainContainer = document.querySelector("#root");
