eb74a98347689f497df1f89a056aa02438b512d4
Himanshu <himanshuraj1209@gmail.com>
2024-03-05 23:33:40 +0530

    [UPD] updated

---
+++ vite-project/README.md
@@ -0,0 +1,3 @@
+# why we use hook ?
+
+- Hooks is use to update the view on changes in data.
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let counter = 5;
+  const addValue = () => {
+    // console.log("value added", Math.random())
+    console.log("counter---------", counter)
+    counter = counter + 1
+  }
+  return (
+    <>
+      <h1>Chai or react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+        onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
